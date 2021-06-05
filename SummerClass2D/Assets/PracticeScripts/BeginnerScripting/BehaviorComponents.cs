3b62cf6995a70e9c2a198d2a0b42a60da0edeb6e
Danielle Green <dangre.pine@gmail.com>
2021-06-04 23:26:18 -0600

    BeginScript - Behavior Components

---
+++ SummerClass2D/Assets/PracticeScripts/BeginnerScripting/BehaviorComponents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorComponents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Changes sprite color to red
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        // Changes sprite color to green
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        // Changes sprite color to blue
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
