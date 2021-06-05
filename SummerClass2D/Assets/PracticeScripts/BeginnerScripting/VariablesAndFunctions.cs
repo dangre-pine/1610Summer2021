a28582dbb53e3aa269f70bda46106c60b2822de9
Danielle Green <dangre.pine@gmail.com>
2021-06-04 23:33:40 -0600

    BeginScript - Variables and Functions

---
+++ SummerClass2D/Assets/PracticeScripts/BeginnerScripting/VariablesAndFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // debug.log prints results
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+    
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
