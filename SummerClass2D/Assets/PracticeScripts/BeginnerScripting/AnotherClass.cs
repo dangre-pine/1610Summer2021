fb2893bde245270877074001c7418daed15ce1c0
Danielle Green <dangre.pine@gmail.com>
2021-06-09 23:30:35 -0600

    BeginScript - Another Class

---
+++ SummerClass2D/Assets/PracticeScripts/BeginnerScripting/AnotherClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int peaches;
+    public int kiwis;
+
+    private int scissors;
+    private int tape;
+
+    public void FruitMachine(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total: " + answer);
+    }
+
+    private void OfficeSort(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
