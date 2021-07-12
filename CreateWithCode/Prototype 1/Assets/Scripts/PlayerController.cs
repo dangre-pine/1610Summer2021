58d2ab3277999b2205b83b08721aa648834665a3
Danielle Green <dangre.pine@gmail.com>
2021-07-11 21:14:58 -0600

    CwC - 1.2

---
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
+        // we turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
+
