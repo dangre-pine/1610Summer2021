377dda59e38f097633ab71df5124c5228630b3fe
Danielle Green <dangre.pine@gmail.com>
2021-07-11 23:10:22 -0600

    CwC - 1.4

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.TerrainAPI;
+// using UnityEngine.Experimental.TerrainAPI;
 
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
-    private float turnSpeed;
+    private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
     
@@ -28,8 +28,8 @@ public class PlayerController : MonoBehaviour
         
         // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
         
-        // we turn the vehicle
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        // we turn the vehicle - yo she aint working tho - fixed it grrrr
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
 
