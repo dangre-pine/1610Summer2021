369793dc12f69f36611b315f3fee946c5d62070c
Danielle Green <dangre.pine@gmail.com>
2021-07-14 23:59:03 -0600

    Practice - Ball Generator

---
+++ SummerClass2D/Assets/PracticeScripts/BallGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+    private int number = 0;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (number < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            //Instantiate(ball);
+            number++;
+            Debug.Log(number);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
