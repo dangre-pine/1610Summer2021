a47c52d1034da8dd2b8789eacba1a22cf7ce79d2
Danielle Green <dangre.pine@gmail.com>
2021-07-27 22:02:34 -0600

    CwC - 2.1

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.00f;
+    public float xRange = 10.00f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
