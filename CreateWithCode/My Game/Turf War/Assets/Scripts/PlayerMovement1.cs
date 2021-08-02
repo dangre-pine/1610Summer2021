0cf828b5cebd7bf59ab07545e2ac0210c97dbef4
Danielle Green <dangre.pine@gmail.com>
2021-08-01 22:52:32 -0600

    MyGame - WIP1 - Setting Up Prototype

---
+++ CreateWithCode/My Game/Turf War/Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+ 
+ 
+    private Rigidbody2D playerRb;
+    private Transform playerRotation;
+   
+ 
+    // Use this for initialization
+    void Awake()
+    {
+        playerRotation = GetComponent<Transform>();
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+        ContinuousMovement();
+    }
+ 
+    void Update()
+    {
+        MoveRotation();
+    }
+ 
+    public void ContinuousMovement()
+    {
+       
+        playerRb.MovePosition(transform.position + transform.forward * moveSpeed * Time.deltaTime);
+       
+       
+    }
+    void MoveRotation()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {          
+            playerRotation.rotation = Quaternion.Euler(0f, 180f, 0f);            
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {  
+            playerRotation.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {      
+            playerRotation.rotation = Quaternion.Euler(0f, -90f, 0f);                      
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            playerRotation.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+    }
+}
