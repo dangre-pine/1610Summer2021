e1a44db38085e5af78184e548fd486e13d196000
Danielle Green <dangre.pine@gmail.com>
2021-08-04 23:03:45 -0600

    CwC - 5.3

--- CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
+++ CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
@@ -5,18 +5,24 @@ using System.Security.Cryptography;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
     
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
     
@@ -27,7 +33,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -39,5 +45,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
