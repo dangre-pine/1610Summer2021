3d4e243528e2ff54a38e65a2e15726292b65cc43
Danielle Green <dangre.pine@gmail.com>
2021-08-04 22:00:20 -0600

    CwC - 5.1

---
+++ CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
